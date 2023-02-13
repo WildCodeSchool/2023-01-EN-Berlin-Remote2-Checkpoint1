2fa40fde7c2b57333b35bddf1344005dbca016d8
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2023-02-13 15:31:02 +0100

    solve point 3 of JS in part 2 (portfolio) advanced
    this is an advanced way of doing the same thing but while
    requiring less from the Javascript using a utility function
    borrowed from Stackoverflow called "upTo"

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,11 +1,9 @@
-
 const nodeAvatar = document.querySelector(".introduction > img");
 
 // Use addEventListener is the most flexible and modern way of doing things
-nodeAvatar.addEventListener(
-    'click',
-    function() { nodeAvatar.src = "./image/avatar.svg"; }
-);
+nodeAvatar.addEventListener("click", function () {
+    nodeAvatar.src = "./image/avatar.svg";
+});
 
 function changeName() {
     const nodeName = document.querySelector("#name");
@@ -15,6 +13,31 @@ function changeName() {
     // As part of point 3 we are also asked to
     // change the background color of the ping-bg
     // class container
-    const nodeContainer = document.querySelector(".pink-bg");
-    nodeContainer.style.backgroundColor = "#750ff7"
+    let nodeContainer = upTo(nodeName, "DIV");
+    nodeContainer.style.backgroundColor = "#750ff7";
+}
+
+/**
+ * Function copied from Stackoverflow:
+ * https://stackoverflow.com/questions/6856871/getting-the-parent-div-of-element
+ *
+ * @param {HTMLElement} el : DOM node of element
+ * @param {string} tagName : tag name of ancestor
+ * @returns first ancestor with tag or undefined if not found
+ */
+function upTo(el, tagName) {
+    tagName = tagName.toLowerCase();
+
+    while (el && el.parentNode) {
+        el = el.parentNode;
+        if (el.tagName && el.tagName.toLowerCase() == tagName) {
+            return el;
+        }
+    }
+
+    // Many DOM methods return null if they don't
+    // find the element they are searching for
+    // It would be OK to omit the following and just
+    // return undefined
+    return null;
 }
