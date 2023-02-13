96d5c062080a633adeeff6a898ea6f122d1e4338
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2023-02-13 15:50:37 +0100

    solve point 4 of JS in part 2 (portfolio)

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -13,6 +13,17 @@ function changeName() {
     // As part of point 3 we are also asked to
     // change the background color of the ping-bg
     // class container
-    const nodeContainer = document.querySelector(".pink-bg");
-    nodeContainer.style.backgroundColor = "#750ff7"
+    const nodeContainers = document.querySelectorAll(".pink-bg");
+    
+    for (let index = 0; index < nodeContainers.length; index++) {
+        const element = nodeContainers[index];
+        element.style.backgroundColor = "#750ff7"
+    }
+
+    const nodeTexts = document.querySelectorAll(".pink-text");
+
+    for (let index = 0; index < nodeTexts.length; index++) {
+        const element = nodeTexts[index];
+        element.style.color = "#750ff7"
+    }
 }
