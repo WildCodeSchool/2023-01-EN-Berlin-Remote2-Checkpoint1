3dfdbc851e49aaecc08872272b3e0d40935fc435
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2023-02-13 15:45:25 +0100

    revert to simpler solution of point 3 in prep for point 4

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -13,31 +13,6 @@ function changeName() {
     // As part of point 3 we are also asked to
     // change the background color of the ping-bg
     // class container
-    let nodeContainer = upTo(nodeName, "DIV");
-    nodeContainer.style.backgroundColor = "#750ff7";
-}
-
-/**
- * Function copied from Stackoverflow:
- * https://stackoverflow.com/questions/6856871/getting-the-parent-div-of-element
- *
- * @param {HTMLElement} el : DOM node of element
- * @param {string} tagName : tag name of ancestor
- * @returns first ancestor with tag or undefined if not found
- */
-function upTo(el, tagName) {
-    tagName = tagName.toLowerCase();
-
-    while (el && el.parentNode) {
-        el = el.parentNode;
-        if (el.tagName && el.tagName.toLowerCase() == tagName) {
-            return el;
-        }
-    }
-
-    // Many DOM methods return null if they don't
-    // find the element they are searching for
-    // It would be OK to omit the following and just
-    // return undefined
-    return null;
+    const nodeContainer = document.querySelector(".pink-bg");
+    nodeContainer.style.backgroundColor = "#750ff7"
 }
